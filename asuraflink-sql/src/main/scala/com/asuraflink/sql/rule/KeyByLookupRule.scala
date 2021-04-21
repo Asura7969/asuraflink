418c1a6d034063f9891c7e391f98e274eef8e28c
gongwenzhou <gongwenzhou@hellobike.com>
2021-04-21 10:12:16 +0800

    [add]-flink sql lookup join with keyby

--- asuraflink-sql/src/main/scala/com/asuraflink/sql/rule/KeyByLookupRule.scala
+++ asuraflink-sql/src/main/scala/com/asuraflink/sql/rule/KeyByLookupRule.scala
@@ -2,24 +2,19 @@ package com.asuraflink.sql.rule
 
 import org.apache.calcite.plan.{RelOptRule, RelOptRuleCall}
 import org.apache.calcite.plan.RelOptRule.{any, operand}
-import org.apache.flink.table.planner.delegation.StreamPlanner
 import org.apache.flink.table.planner.plan.`trait`.FlinkRelDistribution
 import org.apache.flink.table.planner.plan.nodes.FlinkConventions
-import org.apache.flink.table.planner.plan.nodes.exec.ExecNode
-import org.apache.flink.table.planner.plan.nodes.physical.stream.{StreamExecExchange, StreamExecLookupJoin}
+import org.apache.flink.table.planner.plan.nodes.physical.stream.StreamExecLookupJoin
 import org.apache.flink.table.planner.plan.rules.physical.FlinkExpandConversionRule.satisfyDistribution
 
-import scala.collection.JavaConverters._
-import scala.collection.mutable
 
 /**
  * @author asura7969
  * @create 2021-04-18-16:00
  */
-class KeyByLookupRule
-  extends RelOptRule(
-    operand(classOf[StreamExecLookupJoin], any()),
-      "KeyByLookupRule"){
+class KeyByLookupRule extends RelOptRule(
+  operand(classOf[StreamExecLookupJoin], any()),
+  "KeyByLookupRule"){
 
 
   override def onMatch(call: RelOptRuleCall): Unit = {
@@ -27,22 +22,26 @@ class KeyByLookupRule
     val cluster = execLookupJoin.getCluster
     val joinType = execLookupJoin.joinType
     val joinInfo = execLookupJoin.joinInfo
-    val inputNodes: mutable.Seq[ExecNode[StreamPlanner, _]] = execLookupJoin.getInputNodes.asScala
 
     val keys = joinInfo.leftKeys
 
     val newInput = satisfyDistribution(
       FlinkConventions.STREAM_PHYSICAL, execLookupJoin.getInput, FlinkRelDistribution.hash(keys, requireStrict = true))
 
-    val a = new StreamExecLookupJoin(
-      cluster,
-      execLookupJoin.getTraitSet,
-      newInput,
-      // TODO: 需要 temporalTable
-      execLookupJoin.getTable,
-      Option.empty,
-      joinInfo, joinType)
-
-    call.transformTo(a)
+//    val a = new StreamExecLookupJoin(
+//      cluster,
+//      execLookupJoin.getTraitSet,
+//      newInput,
+//      // TODO: 需要 temporalTable
+//      execLookupJoin.getTemporalTable,
+//      execLookupJoin.getCalcOnTemporalTable,
+//      joinInfo, joinType)
+//
+//    call.transformTo(a)
   }
 }
+
+object KeyByLookupRule {
+  val INSTANCE = new KeyByLookupRule
+}
+
