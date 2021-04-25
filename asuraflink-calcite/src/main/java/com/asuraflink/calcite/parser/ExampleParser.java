4e4000b15f3dde849353e300c16a05c3034bd15a
asura7969 <1402357969@qq.com>
2021-04-25 23:47:45 +0800

    [add]-flink-calcite

--- asuraflink-calcite/src/main/java/com/asuraflink/calcite/parser/ExampleParser.java
+++ asuraflink-calcite/src/main/java/com/asuraflink/calcite/parser/ExampleParser.java
@@ -4,6 +4,7 @@ import org.apache.calcite.avatica.util.Casing;
 import org.apache.calcite.avatica.util.Quoting;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.parser.impl.ExampleSqlParserImpl;
 import org.apache.calcite.sql.parser.impl.SqlParserImpl;
 import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.tools.FrameworkConfig;
@@ -16,18 +17,20 @@ import org.apache.calcite.tools.Frameworks;
 public class ExampleParser {
 
     public static void main(String[] args) {
-        final FrameworkConfig config = Frameworks.newConfigBuilder()
+        FrameworkConfig config = Frameworks.newConfigBuilder()
                 .parserConfig(SqlParser.configBuilder()
-                        .setParserFactory(SqlParserImpl.FACTORY)
+                        .setParserFactory(ExampleSqlParserImpl.FACTORY)
+                        // 设置大小写是否敏感
                         .setCaseSensitive(false)
+                        // 设置应用标识,mysql是``
                         .setQuoting(Quoting.BACK_TICK)
+                        // Quoting策略,不变,变大写或变小写
                         .setQuotedCasing(Casing.TO_UPPER)
+                        // 标识符没有被Quoting后的策略
                         .setUnquotedCasing(Casing.TO_UPPER)
-                        .setConformance(SqlConformanceEnum.ORACLE_12)
                         .build())
                 .build();
-
-        String sql = "select ids, name from test where id < 5 and name = 'zhang'";
+        String sql = "run example 'select ids, name from test where id < 5'";
         SqlParser parser = SqlParser.create(sql, config.getParserConfig());
         try {
             SqlNode sqlNode = parser.parseStmt();
